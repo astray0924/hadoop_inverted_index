32eea6521d2fdf59f6b21bb86539399b40456e19
kyoungrok <kyoungrok.jang@gmail.com>
2014-04-10 23:01:01 +0900

    Reducer 작성중

--- src/main/java/invertedindex/InvertedIndexer.java
+++ src/main/java/invertedindex/InvertedIndexer.java
@@ -28,19 +28,22 @@ public class InvertedIndexer {
 
 	public static class InvertedIndexerMapper extends
 			Mapper<LongWritable, Text, LongWritable, Text> {
-		private static final Pattern sanitizePattern = Pattern.compile("[^A-Za-z0-9 ]");
-		private static final Pattern pageIdPattern = Pattern.compile("<id>(.+?)</id>");
+		private static final Pattern sanitizePattern = Pattern
+				.compile("[^A-Za-z0-9 ]");
+		private static final Pattern pageIdPattern = Pattern
+				.compile("<id>(.+?)</id>");
 
 		@Override
 		public void map(LongWritable key, Text value, Context context)
 				throws IOException, InterruptedException {
 			String page = value.toString();
-			
+
 			String pageIdString = getPageID(page);
-			
+
 			String sanitizedPage = getSanitizedPage(page);
-			
-			StringTokenizer iter = new StringTokenizer(sanitizedPage.toLowerCase());
+
+			StringTokenizer iter = new StringTokenizer(
+					sanitizedPage.toLowerCase());
 
 			while (iter.hasMoreTokens()) {
 				Text word = new Text();
@@ -48,8 +51,8 @@ public class InvertedIndexer {
 
 				LongWritable pageId = new LongWritable(new Long(pageIdString));
 
-				if (stopWords.contains(word)) {
-					context.write(pageId, word);					
+				if (!stopWords.contains(word)) {
+					context.write(pageId, word);
 				}
 			}
 		}
@@ -57,7 +60,7 @@ public class InvertedIndexer {
 		public String getSanitizedPage(String page) {
 			return sanitizePattern.matcher(page).replaceAll("");
 		}
-		
+
 		public String getPageID(String page) {
 			Matcher matcher = pageIdPattern.matcher(page);
 			matcher.find();
@@ -65,25 +68,37 @@ public class InvertedIndexer {
 		}
 	}
 
+	// TODO: <word>: <page IDs> => 이 형태로 출력해야 
 	public static class InvertedIndexerReducer extends
-			Reducer<LongWritable, Text, Text, List<IntWritable>> {
+			Reducer<LongWritable, Text, Text, List<LongWritable>> {
 
 		@Override
 		public void reduce(LongWritable key, Iterable<Text> values,
 				Context context) {
-
+			List<LongWritable> pageIdList = new ArrayList<LongWritable>();
+			
+			
+			
+//			ArrayList<Text> list = new ArrayList<Text>();
+//			for (Text val : values) {
+//				list.add(new Text(val));
+//			}
+//			context.write(
+//					key,
+//					new MyArrayWritable(IntWritable.class, list
+//							.toArray(new IntWritable[list.size()])));
 		}
 
 	}
 
-	class PageIDArrayWritable extends ArrayWritable {
+	class PageIdArrayWritable extends ArrayWritable {
 
-		public PageIDArrayWritable(Class<? extends Writable> valueClass,
+		public PageIdArrayWritable(Class<? extends Writable> valueClass,
 				Writable[] values) {
 			super(valueClass, values);
 		}
 
-		public PageIDArrayWritable(Class<? extends Writable> valueClass) {
+		public PageIdArrayWritable(Class<? extends Writable> valueClass) {
 			super(valueClass);
 		}
 
@@ -133,7 +148,7 @@ public class InvertedIndexer {
 		job.setMapOutputValueClass(Text.class);
 
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(PageIDArrayWritable.class);
+		job.setOutputValueClass(PageIdArrayWritable.class);
 
 		job.waitForCompletion(true);
 
